dc693758560ca1a998b39453525c66d2127962ff
jpn254960 <111208020+jpn254960@users.noreply.github.com>
2024-01-12 17:53:54 -0300

    atualização
    clovis e pudim

--- projeto_jogo_clovis e pudim/clovis.js
+++ projeto_jogo_clovis e pudim/clovis.js
@@ -2,6 +2,15 @@ var fome=document.getElementById('fome')
 var sede=document.getElementById('sede')
 var valor_fome=100
 var valor_sede=100
+var clovis=document.getElementById('clovis')
+var numero=10
+
+function piscar(){
+    clovis.src='clovis_piscando.gif'
+    setTimeout(function piscar(){
+        clovis.src='clovis_normal.png'
+    },1100)
+}
 function dimunir_fome(){
 if (valor_fome!=-1){
     fome.innerText=`${valor_fome--}%`
@@ -12,5 +21,8 @@ function dimunir_sede(){
         sede.innerText=`${valor_sede--}%`
     }
 }
+setInterval(piscar,1000*numero)
+
 setInterval(dimunir_sede,5*60*1000)
-setInterval(dimunir_fome,2*60*1000)
+
+setInterval(dimunir_fome,2*60000)
