c09e96365e4f61b8c86e71d250d563b84bc7e77e
jpn254960 <111208020+jpn254960@users.noreply.github.com>
2024-01-13 02:54:19 -0300

    nova atualizção sistema de joguinho
    www

--- projeto_jogo_clovis e pudim/clovis.js
+++ projeto_jogo_clovis e pudim/clovis.js
@@ -1,9 +1,12 @@
 var fome=document.getElementById('fome')
 var sede=document.getElementById('sede')
-var valor_fome=100
-var valor_sede=100
+var moeda=document.getElementById('money')
 var clovis=document.getElementById('clovis')
-var numero=10
+var lista_jogos=document.getElementById('lista_jogos')
+var cenas=[document.getElementById('jogo_1')]
+var quadrado=document.getElementById('quadrado')
+var clovis_jogo_1=document.getElementById('clovis_jogo_1')
+var txt_perdeu=document.getElementById('derrota')
 
 function piscar(){
     clovis.src='clovis_piscando.gif'
@@ -12,17 +15,61 @@ function piscar(){
     },1100)
 }
 function dimunir_fome(){
-if (valor_fome!=-1){
-    fome.innerText=`${valor_fome--}%`
+if (fome.innerText!=0){
+    fome.innerText--
 }
 }
 function dimunir_sede(){
-    if (valor_sede!=-1){
-        sede.innerText=`${valor_sede--}%`
+    if (sede.innerText!=0){
+        sede.innerText--
     }
 }
-setInterval(piscar,1000*numero)
+function comer(){
+    if (moeda.innerText>=2 && fome.innerText!=100){  
+        moeda.innerText-=2
+        fome.innerText++
+    }
+}
+function beber(){
+    if (moeda.innerText>=5 && sede.innerText!=100 ){
+        moeda.innerText-=5
+        sede.innerText++
+    }
+}
+function jogar(){
+    if(lista_jogos.style.visibility=='visible'){
+        lista_jogos.style.visibility='collapse'
+    }else{
+        lista_jogos.style.visibility='visible'
+    }
+}
+function iniciar_jogo(n){
+    cenas[n].style.visibility='visible'
+    if (n==0){
+        quadrado.style.animation='quadrado 1s linear infinite'
+    }
+}
 
-setInterval(dimunir_sede,5*60*1000)
+function pular(){
+        clovis_jogo_1.style.animation='personagem 500ms infinite'
+        setTimeout(function(){
+            clovis_jogo_1.style.animation=''
+        },500)
+}
+function analise(){
+    var top =parseInt(window.getComputedStyle(clovis_jogo_1).getPropertyValue('top'))
+    var left=parseInt(window.getComputedStyle(quadrado).getPropertyValue('left'))
+    console.log(top)
+    if (left>=0 && left<=40 && top>=120){
+        quadrado.style.animation=''
+        clovis_jogo_1.style.animation=''
+        txt_perdeu.style.visibility='visible'
+
+    }
+}
+setInterval(analise,10)
+setInterval(piscar,1000*10)
+
+setInterval(dimunir_sede,5*60000)
 
 setInterval(dimunir_fome,2*60000)
